8bfed5685f69519aab1600a11cf98636a66bbf80
juliano <julianoh1909@gmail.com>
2023-12-05 18:45:33 -0300

    initial structure. set alias. svelte 5 with runes

---
+++ src/components/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="/tutorial/dark-theme.css" />
+</svelte:head>
+
+<div class="panel"></div>
+
+<style>
+    .panel {
+        padding: 3rem;
+        background: rgb(0, 0, 0, 0.5);
+        width: 300px;
+        height: 300px;
+    }
+</style>
